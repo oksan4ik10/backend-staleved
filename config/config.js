4e91b7b7ac0a99bdca1dd16be13c4b25717d61aa
Kalinina Oksana <ksan4ik92@gmail.com>
2024-02-12 18:57:32 +0300

    feat: auth

--- config/config.js
+++ config/config.js
@@ -1,3 +1,5 @@
 module.exports = {
     ATLAS_URI: 'mongodb+srv://admin:1234@cluster0.9dw2jf9.mongodb.net/staleved',
+    keys: 'dev-jwt',
+    keysReset: 'res-jwt'
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,36 @@
-module.exports.login = (req, res)=> {
-    res.status(200).json({
-        login: {
-            password: req.body.password
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+
+const config = require('../config/config')
+const Worker = require("../models/Worker")
+
+module.exports.login = async (req, res)=> {
+    try{
+        const candidate = await Worker.findOne({login: req.body.login});
+        if(candidate){
+            const passwordRes = bcrypt.compareSync(req.body.password, candidate.password)
+            if(passwordRes){
+                const token= jwt.sign({
+                    userId: candidate._id
+                }, config.keys, {expiresIn: 60*60*24*365});
+                res.status(200).json({
+                    token: `Bearer ${token}`
+                })
+            } else{
+                res.status(401).json({
+                    message: "Пароль не верный. Попробуйте снова"
+                })
+            }
+
+        } else{
+            res.status(404).json({
+                message: "Пользователь с таким логином не найден"
+            })
         }
-    })
+    }
+    catch(e){
+        console.log(e);
+        // errorHandler(res, e)
+    }
 }
 
